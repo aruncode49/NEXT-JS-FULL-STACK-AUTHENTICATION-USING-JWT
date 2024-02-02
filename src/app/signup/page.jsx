840b825fcbad28fb3322405a9396240cd718d1bb
Arun Kumar <aruncode49@gmail.com>
2024-02-02 14:00:34 +0530

    sign up form design complete

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,5 +1,44 @@
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+    <div className="flex flex-col justify-center items-center min-h-[100vh]">
+      <h1 className="text-3xl font-bold">Sign Up</h1>
+      <form className="flex flex-col gap-5 mt-5" action="">
+        <span className="flex flex-col gap-1">
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            className="outline-none border-none px-2 py-1 rounded-md text-black"
+            type="text"
+            name="fullName"
+            placeholder="Full Name"
+          />
+        </span>
+
+        <span className="flex flex-col gap-1">
+          <label htmlFor="email">Email</label>
+          <input
+            className="outline-none border-none px-2 py-1 rounded-md text-black"
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+        </span>
+
+        <span className="flex flex-col gap-1">
+          <label htmlFor="password">Password</label>
+          <input
+            className="outline-none border-none px-2 py-1 rounded-md text-black"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </span>
+
+        <button className="w-full bg-blue-600 p-1 rounded-md mt-3">
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignupPage;
