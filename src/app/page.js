3addea875a2882418f2456a960a5167d20c7d6a1
Arun Kumar <aruncode49@gmail.com>
2024-02-02 15:19:20 +0530

    created user model

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="">
+    <main className="min-h-[100vh] flex flex-col justify-center items-center">
       <h1>Hello Next JS</h1>
     </main>
   );
